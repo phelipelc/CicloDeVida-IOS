aed20e3f3e25e9c5552405797d52762c83eec3a5
phelipelc <phelipelcampos@hotmail.com>
2018-01-15 11:53:14 -0200

    Log de mudança de estados Aplicativo e Views

--- CicloDeVida-IOS/ViewController.swift
+++ CicloDeVida-IOS/ViewController.swift
@@ -10,15 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtField: UITextField!
+    @IBOutlet weak var lbltext: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Tela 1: viewDidLoad")
+        lbltext.text = "Entre com seu nome"
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Tela 1: viewWillAppear")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Tela 1: viewDidAppear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("Tela 1: viewWillDisappear")
+       lbltext.text = txtField.text
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("Tela 1: viewDidDisappear")
+    }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
